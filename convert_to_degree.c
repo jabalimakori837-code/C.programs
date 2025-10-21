2a9b5c5c65e9b91c2bb70e3959f896c101ef4671
jabalimakori837-code <jabalimakori837@gmail.com>
2025-10-21 14:23:17 +0300

    Add files via upload

---
+++ convert_to_degree.c
@@ -0,0 +1,24 @@
+/*
+NAME : Jabali Kayago 
+Reg : PA106\G\28768\25
+DESCRIPTION : A PROGRAM THAT TRANFERS TEMPRETURE FROM DEGREES TO FAHRENHEIT 
+
+*/
+#include<stdio.h>
+
+int main()
+{
+float fahrenheit;
+float degree;
+
+printf("Enter the temperature in fahrenheit :");
+scanf("%f", &fahrenheit);
+
+degree = ( fahrenheit - 32 ) * 5/9 ;
+
+printf("\n Your temperature in degree is\n ");
+printf("%f",degree);
+
+
+return 0 ;
+}
